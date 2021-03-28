770bf8f98df8df37fa48f71204918f085749749d
aemre <aemre38@hotmail.com>
2021-03-28 15:12:40 +0300

    son

--- app.js
+++ app.js
@@ -4,25 +4,29 @@ class Nokta {
         this.y = y;
     }
 }
-uzunluk = (kose1,kose2) => {
-    return Math.sqrt(((kose2.x-kose1.x)**2) + ((kose2.y-kose1.y)**2));
+class Cokgen {
+    constructor(...koseler){ 
+        this.noktalar = koseler;
+    }
+
+    uzunluk = (kose1,kose2) => {
+        return Math.sqrt(((kose2.x-kose1.x)**2) + ((kose2.y-kose1.y)**2));
+    }
+    
+    cevre = () => {
+        let toplam=0;
+        if (this.noktalar.length >= 2) {
+            for (var i=0; i < this.noktalar.length-1; i++) {
+                toplam += this.uzunluk(this.noktalar[i],this.noktalar[i+1]);
+            };
+            if (this.noktalar.length > 2)
+                toplam += this.uzunluk(this.noktalar[0],this.noktalar[this.noktalar.length-1]);        
+        };
+        return toplam;
+    }
 }
 
-let noktalar = [];
-toplamMesafe = (noktalar) => {
-    let toplam=0;
-    if (noktalar.length >= 2) {
-        for (var i=0; i < noktalar.length-1; i++) {
-            toplam += uzunluk(noktalar[i],noktalar[i+1]);
-        }
-        if (noktalar.length > 2)
-            toplam += uzunluk(noktalar[0],noktalar[noktalar.length-1]);        
-    }
-    return toplam;
-}
 //4+4+3+3=14 seklindeki dikdortgenin kose noktalari asagidadir.
-noktalar.push(new Nokta(1,1));
-noktalar.push(new Nokta(5,1));
-noktalar.push(new Nokta(5,4));
-noktalar.push(new Nokta(1,4));
-console.log(toplamMesafe(noktalar));
+let dikdortgen = new Cokgen(new Nokta(1,1), new Nokta(5,1), new Nokta(5,4), new Nokta(1,4))
+
+console.log(dikdortgen.cevre());
---
+++ app2.js
@@ -0,0 +1,29 @@
+class Nokta {
+    constructor(x,y){
+        this.x = x;
+        this.y = y;
+    }
+}
+uzunluk = (kose1,kose2) => {
+    return Math.sqrt(((kose2.x-kose1.x)**2) + ((kose2.y-kose1.y)**2));
+}
+
+let noktalar = [];
+toplamMesafe = (noktalar) => {
+    let toplam=0;
+    if (noktalar.length >= 2) {
+        for (var i=0; i < noktalar.length-1; i++) {
+            toplam += uzunluk(noktalar[i],noktalar[i+1]);
+        }
+        if (noktalar.length > 2)
+            toplam += uzunluk(noktalar[0],noktalar[noktalar.length-1]);        
+    }
+    return toplam;
+}
+//4+4+3+3=14 seklindeki dikdortgenin kose noktalari asagidadir.
+noktalar.push(new Nokta(1,1));
+noktalar.push(new Nokta(5,1));
+noktalar.push(new Nokta(5,4));
+noktalar.push(new Nokta(1,4));
+
+console.log(toplamMesafe(noktalar));
